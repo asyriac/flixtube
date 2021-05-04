a5de9edb67d7298164a995406d7a5a87ddf5ec63
Arun Syriac <arun.syriac@outlook.com>
2021-05-04 18:33:51 +0530

    add videolist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes,Route } from "react-router";
 import Navbar from "./components/Navbar/Navbar";
+import VideoList from "./components/VideoList/VideoList";
 
 function App() {
   return (
     <>
      <Navbar />
+     <div className="container">
+       <Routes>
+         <Route path="/" element={<VideoList />}/>
+       </Routes>
+     </div>
     </>
   );
 }
---
+++ src/components/VideoList/VideoList.js
@@ -0,0 +1,39 @@
+import {  useNavigate } from 'react-router-dom';
+import data from '../../data/videos'
+import './VideoList.css'
+
+const VideoList = () => {
+
+
+    const navigate = useNavigate();
+
+    const handleClick = (id) => {
+        navigate(`/video/${id}`)
+    }
+
+    return (
+        <div className="card-list row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1 pt-1">
+            {
+                data.map((item) => {
+                    return (
+                        <div key={item.id} className="card" onClick={() => handleClick(item.id)}>
+                            <img src={item.thumbnail} />
+                            <div className="card-content">
+                                <h3>{item.title}</h3>
+                                <div className="flex mt-md">
+                                    <img src={item.channelImage} className="img-round" />
+                                    <div>
+                                        <span className="ml-sm">{item.channelName}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+
+}
+
+export default VideoList;
