09b24aae4fc791553a3639c7ee4f359824953a84
Arun Syriac <arun.syriac@outlook.com>
2021-07-13 03:03:44 +0530

    add form validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,39 @@
-import { useState } from "react";
 import "./Login.css";
-import { useLocation, Navigate } from "react-router-dom";
+import { useLocation, Navigate, Link } from "react-router-dom";
 import Loading from "../Loading/Loading";
 import useFetchCurrentUser from "../../hooks/useFetchCurrentUser";
 import { useAuthContext } from "../../contexts/auth-context";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
   let { isLoggedIn, initialLoading, loginUser } = useAuthContext();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const { state } = useLocation();
 
   useFetchCurrentUser();
 
   const path = state?.from || "/";
 
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Required"),
+    password: Yup.string().required("Required"),
+  });
+
   const handleLogin = () => {
+    const { username, password } = formik.values;
     loginUser(username, password);
   };
 
-  const resetErrors = () => {
-    // if (serverError !== null) dispatch(resetServerError());
-  };
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleLogin,
+  });
 
   if (initialLoading) return <Loading />;
 
@@ -31,22 +43,12 @@ const Login = () => {
     <div className="flex flex-center login-container">
       <div className="login-form">
         <h3 className="text-center pb-1">Login</h3>
-        {/* {serverError && <div className="alert mb-1">{serverError}</div>} */}
         <div className="form-group">
           <label className="" htmlFor="username">
             Username
           </label>
-          <input
-            className="form-control"
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-              resetErrors();
-            }}
-            autoComplete="off"
-          />
+          <input className="form-control" type="text" id="username" value={formik.values.username} onChange={formik.handleChange} autoComplete="off" onBlur={formik.handleBlur} />
+          {formik.touched.username && formik.errors.username && <span className="invalid-feedback">{formik.errors.username}</span>}
         </div>
         <div className="form-group">
           <label className="" htmlFor="password">
@@ -56,16 +58,21 @@ const Login = () => {
             className="form-control"
             type="password"
             id="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-              resetErrors();
-            }}
+            value={formik.values.password}
+            onChange={formik.handleChange}
             autoComplete="off"
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.password && formik.errors.password && <span className="invalid-feedback">{formik.errors.password}</span>}
         </div>
-        <div className="flex flex-center">
-          <button className="btn btn-secondary btn-sm" onClick={handleLogin}>
+        <span>
+          Don't have an account?{" "}
+          <Link to="/register" className="primary-text">
+            Register.
+          </Link>
+        </span>
+        <div className="flex flex-center mt-1">
+          <button className="btn btn-secondary btn-sm" onClick={formik.handleSubmit} type="button" disabled={!formik.isValid}>
             Login
           </button>
         </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,23 +1,47 @@
-import { useState } from "react";
-import { useLocation, Navigate } from "react-router-dom";
+import { useLocation, Navigate, Link } from "react-router-dom";
 import Loading from "../Loading/Loading";
 import useFetchCurrentUser from "../../hooks/useFetchCurrentUser";
 import { useAuthContext } from "../../contexts/auth-context";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import "./Register.css";
 
 const Register = () => {
   let { isLoggedIn, initialLoading, registerUser } = useAuthContext();
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const { state } = useLocation();
   useFetchCurrentUser();
   const path = state?.from || "/";
 
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email format").required("Required"),
+    username: Yup.string().required("Required"),
+    password: Yup.string().required("Required").min(8, "Password should be 8 chars minimum."),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password", ""), ""], "Passwords must match")
+      .required("Required"),
+  });
+
   const handleRegister = () => {
+    const { firstName, lastName, email, username, password } = formik.values;
     registerUser(firstName, lastName, email, username, password);
   };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleRegister,
+  });
 
   if (initialLoading) return <Loading />;
 
@@ -27,39 +51,78 @@ const Register = () => {
     <div className="flex flex-center login-container">
       <div className="login-form">
         <h3 className="text-center pb-1">Register</h3>
-        <div className="form-group">
-          <label className="" htmlFor="firstname">
-            First Name
-          </label>
-          <input className="form-control" type="text" id="firstname" value={firstName} onChange={(e) => setFirstName(e.target.value)} autoComplete="off" />
-        </div>
-        <div className="form-group">
-          <label className="" htmlFor="lastname">
-            Last Name
-          </label>
-          <input className="form-control" type="text" id="lastname" value={lastName} onChange={(e) => setLastName(e.target.value)} autoComplete="off" />
+        <div className="form-row">
+          <div className="form-group">
+            <label className="" htmlFor="firstName">
+              First Name
+            </label>
+            <input className="form-control" type="text" id="firstName" value={formik.values.firstName} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+            {formik.touched.firstName && formik.errors.firstName && <span className="invalid-feedback">{formik.errors.firstName}</span>}
+          </div>
+          <div className="form-group">
+            <label className="" htmlFor="lastName">
+              Last Name
+            </label>
+            <input className="form-control" type="text" id="lastName" value={formik.values.lastName} onChange={formik.handleChange} autoComplete="off" onBlur={formik.handleBlur} />
+            {formik.touched.lastName && formik.errors.lastName && <span className="invalid-feedback">{formik.errors.lastName}</span>}
+          </div>
         </div>
         <div className="form-group">
           <label className="" htmlFor="email">
             Email
           </label>
-          <input className="form-control" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="off" />
+          <input className="form-control" type="email" id="email" value={formik.values.email} onChange={formik.handleChange} autoComplete="off" onBlur={formik.handleBlur} />
+          {formik.touched.email && formik.errors.email && <span className="invalid-feedback">{formik.errors.email}</span>}
         </div>
         <div className="form-group">
           <label className="" htmlFor="username">
             Username
           </label>
-          <input className="form-control" type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="off" />
+          <input className="form-control" type="text" id="username" value={formik.values.username} onChange={formik.handleChange} autoComplete="off" onBlur={formik.handleBlur} />
+          {formik.touched.username && formik.errors.username && <span className="invalid-feedback">{formik.errors.username}</span>}
         </div>
-        <div className="form-group">
-          <label className="" htmlFor="password">
-            Password
-          </label>
-          <input className="form-control" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="off" />
+        <div className="form-row">
+          <div className="form-group">
+            <label className="" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="form-control"
+              type="password"
+              id="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              autoComplete="off"
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.password && formik.errors.password && <span className="invalid-feedback">{formik.errors.password}</span>}
+          </div>
+          <div className="form-group">
+            <label className="" htmlFor="confirmPassword">
+              Confirm Password
+            </label>
+            <input
+              className="form-control"
+              type="password"
+              id="confirmPassword"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              autoComplete="off"
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.confirmPassword && formik.errors.confirmPassword && <span className="invalid-feedback">{formik.errors.confirmPassword}</span>}
+          </div>
         </div>
-        <div className="flex flex-center">
-          <button className="btn btn-secondary btn-sm" onClick={handleRegister}>
-            Login
+
+        <span>
+          Have an account?{" "}
+          <Link to="/login" className="primary-text">
+            Login.
+          </Link>
+        </span>
+        <div className="flex flex-center mt-1">
+          <button className="btn btn-secondary btn-sm" onClick={formik.handleSubmit} type="submit" disabled={!formik.isValid}>
+            Register
           </button>
         </div>
       </div>
